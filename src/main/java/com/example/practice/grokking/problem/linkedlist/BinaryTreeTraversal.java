022b0b5595f631537e777de208fe57c515750aa9
swarupjana6 <swarup.jana@gmail.com>
2022-02-13 17:12:45 +0530

    Add files via upload

---
+++ src/main/java/com/example/practice/grokking/problem/linkedlist/BinaryTreeTraversal.java
@@ -0,0 +1,22 @@
+package com.example.practice.grokking.problem.linkedlist;
+
+import java.util.List;
+
+public class BinaryTreeTraversal {
+
+	public static void main(String[] args) {
+
+		TreeNode<Integer> root = new TreeNode<>(12);
+	    root.left = new TreeNode<>(7);
+	    root.right = new TreeNode<>(1);
+	    root.left.left = new TreeNode<>(9);
+	    root.right.left = new TreeNode<>(10);
+	    root.right.right = new TreeNode<>(5);
+	    
+	    List<List<Integer>> result = LevelOrderTraversal.traverse(root);
+	    
+	    System.out.println(result);
+	}
+	
+
+}
