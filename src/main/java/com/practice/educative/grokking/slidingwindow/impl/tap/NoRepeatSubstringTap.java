cc671c01a71ec68aeb960fb997ed0985385c2042
tapankumar23 <sabat.tapan@gmail.com>
2022-02-20 18:10:58 +0800

    updated sliding window

--- src/main/java/com/practice/educative/grokking/slidingwindow/impl/tap/NoRepeatSubstringTap.java
+++ src/main/java/com/practice/educative/grokking/slidingwindow/impl/tap/NoRepeatSubstringTap.java
@@ -5,6 +5,17 @@ import com.practice.educative.grokking.slidingwindow.NoRepeatSubstring;
 public class NoRepeatSubstringTap implements NoRepeatSubstring {
     @Override
     public int findLength(String str) {
-        return 0;
+        char[] chars = str.toCharArray();
+        int maxCnt = 0; int count = 0;
+        for(int i = 0; i < chars.length-1; i++) {
+            count++;
+            if(chars[i] == chars[i+1]) {
+                if(count > maxCnt) {
+                    maxCnt = count;
+                    count = 0;
+                }
+            }
+        }
+        return maxCnt;
     }
 }
--- src/test/java/com/practice/educative/grokking/slidingwindow/MaxFruitCountOf2TypesTest.java
+++ src/test/java/com/practice/educative/grokking/slidingwindow/MaxFruitCountOf2TypesTest.java
@@ -1,5 +1,13 @@
 package com.practice.educative.grokking.slidingwindow;
 
+import com.practice.educative.grokking.slidingwindow.impl.tap.MaxFruitCountOf2TypesTap;
+import org.junit.jupiter.api.Test;
+
 class MaxFruitCountOf2TypesTest {
 
+    @Test
+    public static void findLengthTapan() {
+        System.out.println("Length of the longest substring: " + new MaxFruitCountOf2TypesTap().findLength(new char[] { 'A', 'B', 'C', 'A', 'C' }));
+        System.out.println("Length of the longest substring: " + new MaxFruitCountOf2TypesTap().findLength(new char[] { 'A', 'B', 'C', 'B', 'B', 'C' }));
+    }
 }
