3c003e3a4dd6c79175f33a6394d56d1c03324d3d
swarupjana6 <swarup.jana@gmail.com>
2022-02-25 20:04:33 +0530

    delete FilePrint file and its respective reference code

--- src/main/java/com/practice/solid/singleresponsibility/good/Main.java
+++ src/main/java/com/practice/solid/singleresponsibility/good/Main.java
@@ -17,12 +17,9 @@ public class Main {
 		Book book = new Book();
 		TextFormatter formatter = new TextFormatter();
 		ConsolePrint consolePrint = new ConsolePrint();
-		FilePrint filePrint = new FilePrint("D://file.txt");
-		
 		String formattedText = formatter.format(book.getText(), book.getAuthor());
 		
 		consolePrint.print(formattedText);
-		filePrint.print(formattedText);
 		
 	}
 
