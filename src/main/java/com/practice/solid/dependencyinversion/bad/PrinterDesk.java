3a8ba01f56420020b1e236bb43c6b4ea1df0f7e0
tapankumar23 <sabat.tapan@gmail.com>
2022-02-28 23:02:35 +0800

    PrinterDesk updated

--- src/main/java/com/practice/solid/dependencyinversion/bad/PrinterDesk.java
+++ src/main/java/com/practice/solid/dependencyinversion/bad/PrinterDesk.java
@@ -2,14 +2,9 @@ package com.practice.solid.dependencyinversion.bad;
 
 public class PrinterDesk {
 
-    private DotMatrixPrinter printer;
-
-    public PrinterDesk(DotMatrixPrinter printer) {
-        this.printer = printer;
-    }
+    private DotMatrixPrinter printer = new DotMatrixPrinter();
 
     public void printFile() {
         printer.print("File Content");
     }
-
 }
