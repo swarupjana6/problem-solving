c91ea1ba5e0c2662527d043d62981601bd187ef1
tapankumar23 <sabat.tapan@gmail.com>
2022-03-06 13:37:11 +0800

    refactored

--- src/main/java/com/practice/others/string/PreviousGreaterElementIndex.java
+++ src/main/java/com/practice/others/string/PreviousGreaterElementIndex.java
@@ -2,8 +2,11 @@ package com.practice.others.string;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class PreviousGreaterElementIndex {
 
@@ -11,11 +14,13 @@ public class PreviousGreaterElementIndex {
         List<Integer> outputList = new ArrayList<>();
         outputList.add(0);
 
+        int counter = 0;
         for (int i = 1; i < inputList.size(); i++) {
             int j = i - 1;
 
             //Check if previous nums has greater element
             while (j >= 0) {
+                ++counter;
                 if (inputList.get(j) > inputList.get(i)) {
                     outputList.add(i - j);
                     break;
@@ -25,6 +30,7 @@ public class PreviousGreaterElementIndex {
             if (j == -1) outputList.add(i);
         }
 
+        System.out.format("Brut force::  %s\n", counter);
         return outputList;
     }
 
@@ -34,10 +40,13 @@ public class PreviousGreaterElementIndex {
 
         Stack<Integer> prevElements = new Stack<>();
         Stack<Integer> prevElementsIndex = new Stack<>();
+        int counter = 0;
         for (int i = 0; i < n; i++) {
             int currentElement = inputList.get(i);
             nextGreatest[i] = i;
+
             while (!prevElements.empty()) {
+                ++counter;
                 int top = prevElements.peek();
                 int topIndex = prevElementsIndex.peek();
                 if (top > currentElement) {
@@ -48,15 +57,20 @@ public class PreviousGreaterElementIndex {
                     prevElementsIndex.pop();
                 }
             }
+
             prevElements.push(currentElement);
             prevElementsIndex.push(i);
         }
 
+        System.out.format("StackImplementation:: %s\n", counter);
         return Arrays.asList(nextGreatest);
     }
 
     public static void main(String[] args) {
-        List<Integer> inputList = List.of(10, 8, 11, 9, 5, 20, 15, 15);
+        List<Integer> inputList1 = List.of(10, 8, 11, 9, 5, 24, 19, 18, 17, 16, 17, 25);
+        List<Integer> inputList2 = IntStream.rangeClosed(1, 10).boxed().collect(Collectors.toList());
+        List<Integer> inputList3 = IntStream.rangeClosed(1, 10).boxed().sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        List<Integer> inputList = inputList3;
         System.out.println("InputList   :: " + inputList);
         System.out.println("OutputList  :: " + new PreviousGreaterElementIndex().prevMax1(inputList));
         System.out.println("OutputList  :: " + new PreviousGreaterElementIndex().prevMax2(inputList));
