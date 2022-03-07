aaf872a95cf632b25af61ff81a25e64f33bc1891
tapankumar23 <sabat.tapan@gmail.com>
2022-03-07 14:52:54 +0800

    updated

--- src/main/java/com/practice/problems/stack/NearestSmallerOrGreater.java
+++ src/main/java/com/practice/problems/stack/NearestSmallerOrGreater.java
@@ -1,85 +1,127 @@
 package com.practice.problems.stack;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toList;
 
 public class NearestSmallerOrGreater {
 
-    private static List<Integer> nearestGreatest(IntStream stream, List<Integer> inputList, String nearestLabel) {
-        AtomicInteger counter = new AtomicInteger();
-        Stack<Integer> indexStack = new Stack<>();
-        List<Integer> nextGreatest = new LinkedList<>();
+    /*
+     * nearestGreatest and nearestSmallest returns the index list
+     * if we are unable to find then -1
+     *
+     * */
+    public static final String LOG_STR = "\n%s %s\n Time Complexity :: %d \n I/P \t\t:: %s \n O/P Index \t:: %s \n O/P \t\t:: %s \n-------";
 
-        stream.forEach(i -> {
-            counter.incrementAndGet();
-            int resultIndex = i;
-            int current = inputList.get(i);
+    private static void nearestGreatest(int i, List<Integer> inputList, Stack<Integer> indexStack, List<Integer> outputList, AtomicInteger counter) {
+        counter.incrementAndGet();
+        int resultIndex = i;
+        int current = inputList.get(i);
 
-            if (indexStack.size() == 0) resultIndex = i;
-            else if (inputList.get(indexStack.peek()) > current) resultIndex = i - indexStack.peek();
-            else if (inputList.get(indexStack.peek()) <= current) {
-                while (indexStack.size() > 0 && inputList.get(indexStack.peek()) <= current) indexStack.pop();
+        if (indexStack.isEmpty()) resultIndex = -1;
+        else if (inputList.get(indexStack.peek()) > current) resultIndex = indexStack.peek();
+        else if (inputList.get(indexStack.peek()) <= current) {
+            while (!indexStack.isEmpty() && inputList.get(indexStack.peek()) <= current) indexStack.pop();
 
-                if (indexStack.size() == 0) resultIndex = i;
-                else resultIndex = i - indexStack.peek();
-            }
+            if (indexStack.isEmpty()) resultIndex = -1;
+            else resultIndex = indexStack.peek();
+        }
 
-            indexStack.push(i);
-            nextGreatest.add(resultIndex);
-        });
-
-        System.out.format("%s\t|| Time Complexity :: %d\t|| I/P :: %s\t|| O/P :: %s \n", nearestLabel, counter.get(), inputList.toString(), nextGreatest);
-        return nextGreatest;
+        indexStack.push(i);
+        outputList.add(resultIndex);
     }
 
-    private static List<Integer> nearestSmallest(IntStream stream, List<Integer> inputList, String nearestLabel) {
+    private static void nearestSmallest(int i, List<Integer> inputList, Stack<Integer> indexStack, List<Integer> outputList, AtomicInteger counter) {
+        counter.incrementAndGet();
+        int resultIndex = i;
+        int current = inputList.get(i);
+
+        if (indexStack.isEmpty()) resultIndex = -1;
+        else if (inputList.get(indexStack.peek()) < current) resultIndex = indexStack.peek();
+        else if (inputList.get(indexStack.peek()) >= current) {
+            while (!indexStack.isEmpty() && inputList.get(indexStack.peek()) >= current) indexStack.pop();
+
+            if (indexStack.isEmpty()) resultIndex = -1;
+            else resultIndex = indexStack.peek();
+        }
+
+        indexStack.push(i);
+        outputList.add(resultIndex);
+    }
+
+    private static List<Integer> nearestGreatestLeft(List<Integer> inputList) {
         AtomicInteger counter = new AtomicInteger();
         Stack<Integer> indexStack = new Stack<>();
-        List<Integer> nextGreatest = new LinkedList<>();
+        List<Integer> outputList = new LinkedList<>();
 
-        stream.forEach(i -> {
-            counter.incrementAndGet();
-            int resultIndex = i;
-            int current = inputList.get(i);
+        for (int i = 0; i <= inputList.size() - 1; i++)
+            nearestGreatest(i, inputList, indexStack, outputList, counter);
 
-            if (indexStack.size() == 0) resultIndex = i;
-            else if (inputList.get(indexStack.peek()) < current) resultIndex = i - indexStack.peek();
-            else if (inputList.get(indexStack.peek()) >= current) {
-                while (indexStack.size() > 0 && inputList.get(indexStack.peek()) <= current) indexStack.pop();
+        System.out.format(LOG_STR, new Exception().getStackTrace()[0].getMethodName(), "'i' moving from left2right >>>>>>", counter.get(), inputList, outputList, outputList.stream().map(i -> i != -1 ? inputList.get(i) : i).collect(Collectors.toList()));
+        return outputList;
+    }
 
-                if (indexStack.size() == 0) resultIndex = i;
-                else resultIndex = i - indexStack.peek();
-            }
+    private static List<Integer> nearestGreatestRight(List<Integer> inputList) {
+        AtomicInteger counter = new AtomicInteger();
+        Stack<Integer> indexStack = new Stack<>();
+        List<Integer> outputList = new LinkedList<>();
 
-            indexStack.push(i);
-            nextGreatest.add(resultIndex);
-        });
+        for (int i = inputList.size() - 1; i >= 0; i--)
+            nearestGreatest(i, inputList, indexStack, outputList, counter);
 
-        System.out.format("%s\t|| Time Complexity :: %d\t|| I/P :: %s\t|| O/P :: %s \n", nearestLabel, counter.get(), inputList.toString(), nextGreatest);
-        return nextGreatest;
+        Collections.reverse(outputList);
+        System.out.format(LOG_STR, new Exception().getStackTrace()[0].getMethodName(), "'i' moving from right2left <<<<<<<", counter.get(), inputList, outputList, outputList.stream().map(i -> i != -1 ? inputList.get(i) : i).collect(Collectors.toList()));
+        return outputList;
+    }
+
+    private static List<Integer> nearestSmallestLeft(List<Integer> inputList) {
+        AtomicInteger counter = new AtomicInteger();
+        Stack<Integer> indexStack = new Stack<>();
+        List<Integer> outputList = new LinkedList<>();
+
+        for (int i = 0; i <= inputList.size() - 1; i++)
+            nearestSmallest(i, inputList, indexStack, outputList, counter);
+
+        System.out.format(LOG_STR, new Exception().getStackTrace()[0].getMethodName(), " 'i' moving from left2right >>>>>>", counter.get(), inputList, outputList, outputList.stream().map(i -> i != -1 ? inputList.get(i) : i).collect(Collectors.toList()));
+        return outputList;
+    }
+
+    private static List<Integer> nearestSmallestRight(List<Integer> inputList) {
+        AtomicInteger counter = new AtomicInteger();
+        Stack<Integer> indexStack = new Stack<>();
+        List<Integer> outputList = new LinkedList<>();
+
+        for (int i = inputList.size() - 1; i >= 0; i--)
+            nearestSmallest(i, inputList, indexStack, outputList, counter);
+
+        Collections.reverse(outputList);
+        System.out.format(LOG_STR, new Exception().getStackTrace()[0].getMethodName(), "'i' moving from right2left <<<<<<<", counter.get(), inputList, outputList, outputList.stream().map(i -> i != -1 ? inputList.get(i) : i).collect(Collectors.toList()));
+        return outputList;
     }
 
     public static void main(String[] args) {
+        call(Arrays.asList(1, 3, 2, 4));
+        //call(Arrays.asList(1, 3, 0, 0, 1, 2, 4));
+        //call(Arrays.asList(4, 5, 2, 10, 8));
         //call(Arrays.asList(10, 8, 11, 9, 5, 24, 19, 18, 17, 16, 17, 25));
-        call(IntStream.rangeClosed(1, 10).boxed().collect(toList()));
+        //call(IntStream.rangeClosed(1, 10).boxed().collect(toList()));
         //call(IntStream.rangeClosed(1, 10).boxed().sorted(Collections.reverseOrder()).collect(toList()));
     }
 
     private static void call(List<Integer> inputList) {
-        List<Integer> reverseList = new ArrayList<>(inputList);
-        Collections.reverse(reverseList);
-
-        nearestGreatest(IntStream.rangeClosed(0, inputList.size() - 1), new ArrayList<>(inputList), "nearestGreaterToLeft");
-        nearestGreatest(IntStream.rangeClosed(0, inputList.size() - 1), reverseList, "nearestGreaterToRight");
-        nearestSmallest(IntStream.rangeClosed(0, inputList.size() - 1), new ArrayList<>(inputList), "nearestSmallerToLeft");
-        nearestSmallest(IntStream.rangeClosed(0, inputList.size() - 1), reverseList, "nearestSmallerToRight");
+        nearestGreatestRight(inputList);
+        nearestGreatestLeft(inputList);
+        nearestSmallestRight(inputList);
+        nearestSmallestLeft(inputList);
+        //nearestGreatest(new ArrayList<>(inputList), "nearestGreaterToLeft", false);
+        //nearestSmallest(IntStream.rangeClosed(0, inputList.size() - 1), new ArrayList<>(inputList), "nearestSmallerToLeft");
+        //nearestSmallest(IntStream.rangeClosed(0, inputList.size() - 1), inputList, "nearestSmallerToRight");
 
         System.out.println("================================");
     }
--- src/main/java/com/practice/problems/stack/STACK.md
+++ src/main/java/com/practice/problems/stack/STACK.md
@@ -1,14 +1,15 @@
 Stack<a name="TOP"></a>
 ===================
 
-# Nearest greater #
+# Nearest Greatest #
 
 These problems require following things
 1. Stack to store previous indexes
 2. Output Array
 
-## To Right ##
-Traverse from END to START 
+For Right >> Traverse from END to START
+
+For Left >> Traverse from START to END
 
 Steps | Condition                                       | Operation                   
 --- |-------------------------------------------------|-----------------------------
@@ -17,12 +18,19 @@ Steps | Condition                                       | Operation
 3rd | if stack.top LESS_THAN EQUAL_TO current element | then POP() the stack until <ul><li>the stack is empty -> then 1st step</li><li>stack.top GREATER_THAN current element -> then 2nd step</li></ul>
 4th |                                                 | stack.push current index
 
-## To Left ##
-Traverse from START to END
+For Right >> The output is revered
 
-Steps | Condition                                       | Operation
---- |-------------------------------------------------|-----------------------------
-1st | if stack.isEmpty                                | then ADD -1 to Output List
-2nd | if stack.top GREATER_THAN current element       | then ADD stack.top Output List
-3rd | if stack.top LESS_THAN EQUAL_TO current element | then POP() the stack until <ul><li>the stack is empty -> then 1st step</li><li>stack.top GREATER_THAN current element -> then 2nd step</li></ul>
-4th |                                                 | stack.push current index
+# Nearest Smallest #
+
+For Right >> Traverse from END to START
+
+For Left >> Traverse from START to END
+
+Steps | Condition                                          | Operation
+--- |----------------------------------------------------|-----------------------------
+1st | if stack.isEmpty                                   | then ADD -1 to Output List
+2nd | if stack.top LESS_THAN current element             | then ADD stack.top Output List
+3rd | if stack.top GREATER_THAN EQUAL_TO current element | then POP() the stack until <ul><li>the stack is empty -> then 1st step</li><li>stack.top LESS_THAN current element -> then 2nd step</li></ul>
+4th |                                                    | stack.push current index
+
+For Right >> The output is revered
