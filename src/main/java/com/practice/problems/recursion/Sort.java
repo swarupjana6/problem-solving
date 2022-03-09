908a63b60bcc8c554d0366468970fb0393a6e45b
tapankumar23 <sabat.tapan@gmail.com>
2022-03-09 16:51:38 +0800

    Sort added

--- src/main/java/com/practice/problems/recursion/Sort.java
+++ src/main/java/com/practice/problems/recursion/Sort.java
@@ -22,24 +22,40 @@ public class Sort {
         if (input.size() == 0) return;
 
         // HYPOTHESIS
-        int temp = input.get(input.size() - 1);
-        input.remove(input.size() - 1);
+        int temp = input.remove(input.size() - 1);
         sort(input);
 
         // INDUCTION
-        insert(input, temp);
+        // Insert temp that was removed in HYPOTHESIS into the input list
+        // insertIterative(input, temp);       // Instead of using the recursion again we can Iteratively solve the Induction step
+        insertRecursively(input, temp);
     }
 
-    private static void insert(List<Integer> subList, Integer element) {
+    private static void insertRecursively(List<Integer> subList, Integer element) {
+        // BASE CONDITION
         if (subList.size() == 0 || subList.get(subList.size() - 1) <= element) {
             subList.add(subList.size(), element);
             return;
         }
 
-        int temp = subList.get(subList.size() - 1);
-        subList.remove(subList.size() - 1);
+        // HYPOTHESIS
+        int temp = subList.remove(subList.size() - 1);
 
-        insert(subList, element);
+        // INDUCTION
+        insertRecursively(subList, element);
         subList.add(temp);
+    }
+
+    private static void insertIterative(List<Integer> subList, int element) {
+        if (subList.size() == 0 || element >= subList.get(subList.size() - 1)) {
+            subList.add(subList.size(), element);
+            return;
+        }
+        for (int i = subList.size() - 1; i >= 0; i--) {
+            if (element > subList.get(i)) {
+                subList.add(i + 1, element);
+                return;
+            }
+        }
     }
 }
