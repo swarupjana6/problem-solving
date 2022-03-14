333cf55bfc8ad3f8a37b386785b1e94c24905f3c
tapankumar23 <sabat.tapan@gmail.com>
2022-03-14 21:55:41 +0800

    LongestSubstringWithKUniqueChars added

---
+++ src/main/java/com/practice/problems/slidingwindow/variablesize/LongestSubstringWithKUniqueChars.java
@@ -0,0 +1,50 @@
+package com.practice.problems.slidingwindow.variablesize;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Log4j2
+public class LongestSubstringWithKUniqueChars {
+
+    public static void main(String[] args) {
+        String input = "aabacbebebe";
+        int uniqueChar = 3;
+        log.info("Input:{} , uniqueChar:{}", input.toCharArray(), uniqueChar);
+        log.info("Output:{}", slidingWindow(input.toCharArray(), uniqueChar));
+    }
+
+    private static int slidingWindow(char[] input, int uniqueChar) {
+        int answer = 0;
+        int windowStart = 0;
+        int windowEnd = 0;
+        Map<Character, Integer> windowElements = new HashMap<>();
+
+        /** Condition:: # of unique characters in a sliding window **/
+
+        while (windowEnd < input.length) {
+            char currentElement = input[windowEnd];
+
+            windowElements.merge(currentElement, 1, Integer::sum);
+
+            if (windowElements.size() < uniqueChar) {
+                windowEnd++;
+            } else if (windowElements.size() == uniqueChar) {
+                answer = Math.max(answer, windowEnd - windowStart + 1);
+                ++windowEnd;
+            } else if (windowElements.size() > uniqueChar) {
+
+                while (windowElements.size() > uniqueChar) {
+                    windowElements.merge(input[windowStart], -1, Integer::sum);
+                    if (windowElements.get(input[windowStart]) == 0) windowElements.remove(input[windowStart]);
+                    windowStart++;
+                }
+                windowEnd++;
+            }
+
+        }
+
+        return answer;
+    }
+}
