6a473e0a055f937175dd914d54fa249c61c94cab
tapankumar23 <sabat.tapan@gmail.com>
2022-03-14 23:20:15 +0800

    LongestSubstringWithoutRepeatingCharacters added

--- src/main/java/com/practice/problems/slidingwindow/variablesize/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/practice/problems/slidingwindow/variablesize/LongestSubstringWithoutRepeatingCharacters.java
@@ -6,40 +6,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Log4j2
-public class LongestSubstringWithKUniqueChars {
+public class LongestSubstringWithoutRepeatingCharacters {
 
     public static void main(String[] args) {
-        String input = "aabacbebebe";
-        int uniqueChar = 3;
-        log.info("Input:{} , uniqueChar:{}", input.toCharArray(), uniqueChar);
-        log.info("Output:{}", slidingWindow(input.toCharArray(), uniqueChar));
+        String input1 = "pwwkew";
+        String input2 = "aabacbbbebe";
+        String input = input1;
+        log.info("Input:    {}", input.toCharArray());
+        log.info("Output:   {}", slidingWindow(input.toCharArray()));
     }
 
-    private static int slidingWindow(char[] input, int uniqueChar) {
+    private static int slidingWindow(char[] input) {
         int answer = 0;
         int windowStart = 0;
         int windowEnd = 0;
+        int uniqueChar = 0;
         Map<Character, Integer> windowElements = new HashMap<>();
 
         /** Condition:: # of unique characters in a sliding window **/
 
         while (windowEnd < input.length) {
             char currentElement = input[windowEnd];
+            int currentWindowSize = windowEnd - windowStart + 1;
 
             windowElements.merge(currentElement, 1, Integer::sum);
+            uniqueChar = windowElements.size();
 
-            if (windowElements.size() < uniqueChar) {
-                windowEnd++;
-            } else if (windowElements.size() == uniqueChar) {
+            if (currentWindowSize == uniqueChar) {
                 answer = Math.max(answer, windowEnd - windowStart + 1);
                 ++windowEnd;
-            } else if (windowElements.size() > uniqueChar) {
+            } else if (currentWindowSize > uniqueChar) {
 
-                while (windowElements.size() > uniqueChar) {
+                while (currentWindowSize > uniqueChar) {
                     char startElement = input[windowStart];
                     windowElements.merge(startElement, -1, Integer::sum);
                     if (windowElements.get(startElement) == 0) windowElements.remove(startElement);
                     windowStart++;
+                    currentWindowSize--;
                 }
                 windowEnd++;
             }
