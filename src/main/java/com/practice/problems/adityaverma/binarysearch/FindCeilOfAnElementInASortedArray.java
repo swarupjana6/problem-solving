3201eac68c2a0f0f72dc5d0200a5ac6b70a33703
tapankumar23 <sabat.tapan@gmail.com>
2022-03-17 18:20:20 +0800

    FindCeilOfAnElementInASortedArray and FindFloorOfAnElementInASortedArray added

---
+++ src/main/java/com/practice/problems/adityaverma/binarysearch/FindCeilOfAnElementInASortedArray.java
@@ -0,0 +1,68 @@
+package com.practice.problems.adityaverma.binarysearch;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
+
+/**
+ * Given a sorted array and a and a value x, find the ceiling of x in the array.
+ * The ceiling of x is the smallest element in the array greater than or equal to x.
+ * Output 1 if the ceiling doesn’t exist.
+ * <p>
+ * Input : a[] = {1, 3, 9, 15, 15, 18, 21}, x = 0
+ * Output : 1
+ * 1 is the smallest element in the array greater than or equal to 5.
+ * <p>
+ * Input : a[] = {1, 3, 9, 15, 15, 18, 21}, x = 1
+ * Output : 1
+ * 1 is the smallest element in the array greater than or equal to 1.
+ * <p>
+ * Input : a[] = {1, 3, 9, 15, 15, 18, 21}, x = 5
+ * Output : 9
+ * 9 is the smallest element in the array greater than or equal to 5.
+ * <p>
+ * Input : a[] = {1, 3, 9, 15, 15, 18, 21}, x = 25
+ * Output : -1
+ * Ceiling doesn't exist
+ **/
+
+@Log4j2
+public class FindCeilOfAnElementInASortedArray {
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 8, 10, 10, 12, 19);
+        Integer ceilOf = 5;
+
+        log.info("Input: {} | Search for: {}", list, ceilOf);
+        log.info("Output: {} ", binarySearch(list, ceilOf));
+    }
+
+    public static Integer binarySearch(List<Integer> list, Integer ceilOf) {
+        int ceil = Integer.MAX_VALUE;
+        int diff = Integer.MAX_VALUE;
+        int start = 0;
+        int end = list.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;        /* LESS OPTIMIZED --> (start + end) / 2; */
+
+            if (list.get(mid) == ceilOf) return list.get(mid + 1);
+            else if (list.get(mid) < ceilOf) {
+                start = mid + 1;
+            } else if (list.get(mid) > ceilOf) {
+                int currentDiff = Math.abs(ceilOf - list.get(mid));
+                if (currentDiff < diff) {
+                    ceil = list.get(mid);
+                    diff = currentDiff;
+                }
+
+                end = mid - 1;
+            }
+        }
+        return ceil;
+    }
+
+    public static int binarySearchElement(List<Integer> list, Integer search, int start, int end) {
+
+        return -1;
+    }
+}
---
+++ src/main/java/com/practice/problems/adityaverma/binarysearch/FindFloorOfAnElementInASortedArray.java
@@ -0,0 +1,64 @@
+package com.practice.problems.adityaverma.binarysearch;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
+
+/**
+ * Given a sorted array and a and a value x, find the floor of x in the array.
+ * The floor of x is the largest element in the array smaller than or equal to x.
+ * Output 1 if the floor doesn’t exist.
+ * <p>
+ * Input : a[] = {1, 3, 9, 15, 15, 18, 21}, x = 5
+ * Output : 3
+ * 3 is the largest element in the array smaller than or equal to 5.
+ * <p>
+ * Input : a[] = {1, 3, 9, 15, 15, 18, 21}, x = 25
+ * Output : 21
+ * 21 is the largest element in the array smaller than or equal to 25.
+ * <p>
+ * Input : a[] = {1, 3, 9, 15, 15, 18, 21}, x = 0
+ * Output : -1
+ * Since floor doesn't exist, output is -1.
+ **/
+
+@Log4j2
+public class FindFloorOfAnElementInASortedArray {
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 8, 10, 10, 12, 19);
+        Integer floorOf = 5;
+
+        log.info("Input: {} | Search for: {}", list, floorOf);
+        log.info("Output: {} ", binarySearch(list, floorOf));
+    }
+
+    public static Integer binarySearch(List<Integer> list, Integer floorOf) {
+        int floor = Integer.MAX_VALUE;
+        int diff = Integer.MAX_VALUE;
+        int start = 0;
+        int end = list.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;        /* LESS OPTIMIZED --> (start + end) / 2; */
+
+            if (list.get(mid) == floorOf) return list.get(mid);
+            else if (list.get(mid) < floorOf) {
+                int currentDiff = Math.abs(floorOf - list.get(mid));
+                if (currentDiff < diff) {
+                    floor = list.get(mid);
+                    diff = currentDiff;
+                }
+
+                start = mid + 1;
+            } else if (list.get(mid) > floorOf) {
+                end = mid - 1;
+            }
+        }
+        return floor;
+    }
+
+    public static int binarySearchElement(List<Integer> list, Integer search, int start, int end) {
+
+        return -1;
+    }
+}
