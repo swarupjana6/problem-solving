2cfddae063c391244fdc5289070ceec0bbe4f2ae
tapankumar23 <sabat.tapan@gmail.com>
2022-03-21 19:01:19 +0800

    updated

--- src/main/java/com/practice/problems/adityaverma/dynamicprog/knapsack/PrintDPMatrix.java
+++ src/main/java/com/practice/problems/adityaverma/dynamicprog/knapsack/PrintDPMatrix.java
@@ -2,24 +2,28 @@ package com.practice.problems.adityaverma.dynamicprog.knapsack;
 
 public class PrintDPMatrix {
     public static void printMatrix(int[] numbers, int index, boolean[][] results, int expectSum) {
-        System.out.print("#" + "\t\t");
+        System.out.print("#" + "\t|\t");
         System.out.print("0" + "\t\t");
         for (int i = 1; i <= expectSum; i++) System.out.print(i + "\t\t");
         System.out.println();
+        for (int i = 1; i <= expectSum; i++) System.out.print(" - - - - ");
+        System.out.println();
         for (int i = 0; i <= index; i++) {
-            System.out.print((i != 0 ? numbers[i - 1] : "0") + "\t\t");
+            System.out.print((i != 0 ? numbers[i - 1] : "0") + "\t|\t");
             for (int j = 0; j <= expectSum; j++) System.out.print(printChar(results[i][j]) + "\t");
             System.out.println();
         }
     }
 
     public static void printMatrix(int[] numbers, int index, int[][] results, int expectSum) {
-        System.out.print("#" + "\t\t");
+        System.out.print("#" + "\t|\t");
         System.out.print("0" + "\t\t");
         for (int i = 1; i <= expectSum; i++) System.out.print(i + "\t\t");
         System.out.println();
+        for (int i = 1; i <= expectSum; i++) System.out.print(" - - - - ");
+        System.out.println();
         for (int i = 0; i <= index; i++) {
-            System.out.print((i != 0 ? numbers[i - 1] : "0") + "\t\t");
+            System.out.print((i != 0 ? numbers[i - 1] : "0") + "\t|\t");
             for (int j = 0; j <= expectSum; j++) System.out.print(printChar(results[i][j]) + "\t\t");
             System.out.println();
         }
--- src/main/java/com/practice/problems/adityaverma/dynamicprog/knapsack/zeroone/CountOfSubsetSumWithGivenSum.java
+++ src/main/java/com/practice/problems/adityaverma/dynamicprog/knapsack/zeroone/CountOfSubsetSumWithGivenSum.java
@@ -2,6 +2,9 @@ package com.practice.problems.adityaverma.dynamicprog.knapsack.zeroone;
 
 import lombok.extern.log4j.Log4j2;
 
+import java.util.function.Consumer;
+
+import static com.practice.problems.adityaverma.dynamicprog.knapsack.PrintDPMatrix.printMatrix;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -21,55 +24,43 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class CountOfSubsetSumWithGivenSum {
 
     public static void main(String[] args) {
-        int[] numbers;
-        int sum;
+        print(new int[]{2, 3, 5, 6, 8, 10}, 10, count -> assertTrue(3 == count));
+        print(new int[]{2, 3, 5, 6, 8, 10}, 101, count -> assertFalse(3 == count));
+    }
+
+    private static void print(int[] numbers, int sum, Consumer<Integer> expected) {
         int count;
-
-        numbers = new int[]{2, 3, 5, 6, 8, 10};
-        sum = 10;
         log.info("Input:: Numbers: {}\t# Sum: {}", numbers, sum);
         count = solveKnapsack(numbers, sum);
         log.info("Output:: Count of subsets: {}", count);
-        assertTrue(count == 3);
-
-        numbers = new int[]{2, 3, 5, 6, 8, 10};
-        sum = 101;
-        log.info("Input:: Numbers: {}\t# Sum: {}", numbers, sum);
-        count = solveKnapsack(numbers, sum);
-        log.info("Output:: Count of subsets: {}", count);
-        assertFalse(count == 3);
+        expected.accept(count);
     }
 
     public static int solveKnapsack(int[] numbers, int sum) {
-        return knapsack(numbers, sum, numbers.length);
+        int[][] results = knapsack(numbers, sum, numbers.length);
+        return results[numbers.length][sum];
     }
 
-    private static int knapsack(int[] numbers, int sum, int index) {
-        int[][] cachedResult = new int[numbers.length + 1][sum + 1];
+    private static int[][] knapsack(int[] numbers, int sum, int index) {
+        int[][] results = new int[index + 1][sum + 1];
 
-        for (int i = 0; i <= index; i++) {
-            for (int j = 0; j <= sum; j++) {
-                if (j == 0) {
-                    cachedResult[i][j] = 1;
-                    continue;
-                }
+        for (int x = 0; x <= sum; x++) results[0][x] = 0;
+        for (int x = 0; x <= index; x++) results[x][0] = 1;
 
-                if (i == 0 && j != 0) {
-                    cachedResult[i][j] = 0;
-                    continue;
-                }
-
+        for (int i = 1; i <= index; i++) {
+            for (int j = 1; j <= sum; j++) {
                 int indexSum = numbers[i - 1];
 
                 if (indexSum > j) {
-                    cachedResult[i][j] = cachedResult[i - 1][j];
+                    results[i][j] = results[i - 1][j];
                 } else {
-                    cachedResult[i][j] = cachedResult[i - 1][j] + cachedResult[i - 1][j - indexSum];
+                    results[i][j] = results[i - 1][j] + results[i - 1][j - indexSum];
                 }
-
             }
         }
 
-        return cachedResult[index][sum];
+        printMatrix(numbers, index, results, sum);
+
+        return results;
     }
 }
