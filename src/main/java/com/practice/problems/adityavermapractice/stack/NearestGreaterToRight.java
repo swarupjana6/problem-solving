0bf4bda06bc7dcd41ad54f36dc2f5b4bd7d51b41
tapankumar23 <sabat.tapan@gmail.com>
2022-05-15 21:10:14 +0800

    updated

--- src/main/java/com/practice/problems/adityavermapractice/stack/NearestGreaterToRight.java
+++ src/main/java/com/practice/problems/adityavermapractice/stack/NearestGreaterToRight.java
@@ -1,14 +1,23 @@
 package com.practice.problems.adityavermapractice.stack;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface NearestGreaterToRight {
+@Log4j2
+public abstract class NearestGreaterToRight {
 
-    List<Integer> nearestGreaterToRight(List<Integer> inputList, int pseudoIndex);
+    String LOG_STR = "{} {}\n I/P List \t\t\t:: {} \n O/P Indexes \t\t:: {} \n O/P Elements \t\t:: {} \n-------\n";
 
-    default void call() {
+    public abstract List<Integer> nearestGreaterToRight(List<Integer> inputList, int pseudoIndex);
+
+    protected void execute() {
         List<Integer> inputList = Arrays.asList(1, 3, 2, 4);
-        nearestGreaterToRight(inputList, -1);
+        List<Integer> outputList = nearestGreaterToRight(inputList, -1);
+
+        String outputStr = outputList.stream().map(i -> i != -1 && i < outputList.size() ? inputList.get(i) : i).collect(Collectors.toList()).toString();
+        log.debug(LOG_STR, new Exception().getStackTrace()[0].getMethodName(), " 'i' moving from left2right >>>>>>", inputList, outputList, outputStr);
     }
 }
--- src/main/java/com/practice/problems/adityavermapractice/stack/impl/jan/NearestGreaterToRightJana.java
+++ src/main/java/com/practice/problems/adityavermapractice/stack/impl/jan/NearestGreaterToRightJana.java
@@ -4,13 +4,13 @@ import com.practice.problems.adityavermapractice.stack.NearestGreaterToRight;
 
 import java.util.List;
 
-public class NearestGreaterToRightJana implements NearestGreaterToRight {
+public class NearestGreaterToRightJana extends NearestGreaterToRight {
     @Override
     public List<Integer> nearestGreaterToRight(List<Integer> inputList, int pseudoIndex) {
         return null;
     }
 
     public static void main(String[] args) {
-        new NearestGreaterToRightJana().call();
+        new NearestGreaterToRightJana().execute();
     }
 }
