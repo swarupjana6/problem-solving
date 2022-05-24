2dd16861ae9341f17e3ad484ed37ee8166a39b80
tapankumar23 <sabat.tapan@gmail.com>
2022-05-25 01:44:42 +0800

    KnapsackProblems-4

--- src/main/java/com/practice/problems/adityavermapractice/dp/knapsack/KnapsackProblems.java
+++ src/main/java/com/practice/problems/adityavermapractice/dp/knapsack/KnapsackProblems.java
@@ -3,14 +3,6 @@ package com.practice.problems.adityavermapractice.dp.knapsack;
 import java.util.Arrays;
 
 public class KnapsackProblems {
-    public static boolean subsetSumRecursive(int[] inputs, int target) {
-        return SubsetSum.recursion(inputs, target, inputs.length - 1);
-    }
-
-    public static boolean subsetSumTopdown(int[] inputs, int target) {
-        return SubsetSum.topDown(inputs, target);
-    }
-
     public static boolean equalSumPartitionRecursive(int[] inputs) {
         int target = Arrays.stream(inputs).mapToObj(e -> Integer.valueOf(e)).reduce(0, Integer::sum);
         if (target % 2 != 0) return false;
@@ -25,6 +17,15 @@ public class KnapsackProblems {
 }
 
 class SubsetSum {
+
+    public static boolean executeSubsetSumRecursive(int[] inputs, int target) {
+        return recursion(inputs, target, inputs.length - 1);
+    }
+
+    public static boolean executeSubsetSumTopdown(int[] inputs, int target) {
+        return topDown(inputs, target);
+    }
+
     public static boolean recursion(int[] numbers, int sum, int index) {
         if (index == -1) return false;
         if (sum == 0) return true;
@@ -41,6 +42,11 @@ class SubsetSum {
     }
 
     public static boolean topDown(int[] numbers, int expectedSum) {
+        boolean[][] results = subsetSum(numbers, expectedSum);
+        return results[numbers.length][expectedSum];
+    }
+
+    public static boolean[][] subsetSum(int[] numbers, int expectedSum) {
         int orgIndex = numbers.length;
         boolean[][] results = new boolean[orgIndex + 1][expectedSum + 1];
 
@@ -61,8 +67,7 @@ class SubsetSum {
                 }
             }
         }
-
-        return results[orgIndex][expectedSum];
+        return results;
     }
 }
 
@@ -73,5 +78,117 @@ class EqualSumPartition {
 
     public static boolean topDown(int[] numbers, int expectedSum) {
         return SubsetSum.topDown(numbers, expectedSum);
+    }
+}
+
+class CountOfSubsetSum {
+
+    public static int recursion(int[] numbers, int sum, int index) {
+        if (index == -1) return 0;
+        if (sum == 0) return 1;
+
+        int number = numbers[index];
+        if (sum < number) {
+            int exclude = recursion(numbers, sum, index - 1);
+            return exclude;
+        } else {
+            int exclude = recursion(numbers, sum, index - 1);
+            int include = recursion(numbers, sum - number, index - 1);
+            return exclude + include;
+        }
+    }
+
+    public static int topDown(int[] numbers, int expectedSum) {
+        int numbersSum = Arrays.stream(numbers).sum();
+        if (numbersSum < expectedSum) return 0;
+
+        int orgIndex = numbers.length;
+
+        int[][] results = new int[orgIndex + 1][expectedSum + 1];
+
+        for (int index = 0; index <= orgIndex; index++) results[index][0] = 1;
+        for (int sum = 1; sum <= expectedSum; sum++) results[0][sum] = 0;
+
+        for (int index = 1; index <= orgIndex; index++) {
+            for (int sum = 1; sum <= expectedSum; sum++) {
+
+                int num = numbers[index - 1];
+                if (sum < num) {
+                    int exclude = results[index - 1][sum];
+                    results[index][sum] = exclude;
+                } else {
+                    int exclude = results[index - 1][sum];
+                    int include = results[index - 1][sum - num];
+                    results[index][sum] += include + exclude;
+                }
+            }
+        }
+
+        return results[orgIndex][expectedSum];
+    }
+}
+
+class MinimumSubsetSumSequence {
+    public static int topDown(int[] numbers) {
+        int expectedSum = Arrays.stream(numbers).sum();
+        boolean[][] results = SubsetSum.subsetSum(numbers, expectedSum);
+        int min = Integer.MAX_VALUE;
+
+        for (int sum = 0; sum <= expectedSum / 2; sum++) {
+            if (results[numbers.length][sum]) {
+                int diff = (expectedSum - 2 * sum);
+                min = Math.min(min, diff);
+            }
+        }
+        return min;
+    }
+}
+
+class CountOfSubsetSumWithDifference {
+    public static int topDown(int[] numbers, int difference) {
+        int sum = Arrays.stream(numbers).sum();
+        if (sum < difference || (sum + difference) % 2 != 0) return 0;
+        int subset1Sum = getSubset1Sum(difference, sum);
+        return CountOfSubsetSum.topDown(numbers, subset1Sum);
+    }
+
+    public static int recursion(int[] numbers, int difference) {
+        int sum = Arrays.stream(numbers).sum();
+        if (sum < difference || (sum + difference) % 2 != 0) return 0;
+        int subset1Sum = getSubset1Sum(difference, sum);
+        return CountOfSubsetSum.recursion(numbers, subset1Sum, numbers.length - 1);
+    }
+
+    private static int getSubset1Sum(int difference, int sum) {
+        /**
+         subset1Sum + subset2Sum = sum;
+         subset1Sum - subset2Sum = difference;
+         -------------------------------
+         2 * subset1Sum = sum + difference;
+         subset1Sum     = (sum + difference)/2;
+         **/
+        int subset1Sum = (sum + difference) / 2;
+        return subset1Sum;
+    }
+}
+
+class TargetSum {
+    public static int topDown(int[] numbers, int target) {
+        int totalSum = Arrays.stream(numbers).sum();
+        if (totalSum < target || (totalSum + target) % 2 != 0) return 0;
+        int firstSubsetSum = (totalSum + target) / 2;
+        return countOfSum(numbers, firstSubsetSum);
+        //return CountOfSubsetSumWithDifference.topDown(numbers, firstSubsetSum);
+    }
+
+    private static int countOfSum(int[] numbers, int targetSum) {
+        int counts[] = new int[targetSum + 1];
+        counts[0] = 1;
+
+        for (int index = 0; index < numbers.length; index++)
+            for (int sum = targetSum; sum >= numbers[index]; sum--)
+                counts[sum] = counts[sum] + counts[sum - numbers[index]];
+
+        return counts[targetSum];
     }
 }
--- src/main/java/com/practice/problems/adityavermapractice/dp/knapsack/Main.java
+++ src/main/java/com/practice/problems/adityavermapractice/dp/knapsack/Main.java
@@ -3,10 +3,12 @@ package com.practice.problems.adityavermapractice.dp.knapsack;
 import java.util.Arrays;
 import java.util.List;
 
-import static com.practice.problems.adityavermapractice.dp.knapsack.KnapsackProblems.*;
+import static com.practice.problems.adityavermapractice.dp.knapsack.KnapsackProblems.equalSumPartitionRecursive;
+import static com.practice.problems.adityavermapractice.dp.knapsack.KnapsackProblems.equalSumPartitionTopdown;
 import static com.practice.problems.adityavermapractice.dp.knapsack.ParentKnapsack.*;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.practice.problems.adityavermapractice.dp.knapsack.SubsetSum.executeSubsetSumRecursive;
+import static com.practice.problems.adityavermapractice.dp.knapsack.SubsetSum.executeSubsetSumTopdown;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Main {
 
@@ -14,38 +16,76 @@ public class Main {
         executeSolve01Knapsack();
         executeSubsetSum();
         executeEqualSumPartition();
+        executeCountOfSubsetSum();
+        executeMinimumSubsetSumSequence();
+        executeCountOfSubsetSumWithDifference();
+        executeTargetSum();
+    }
+
+    private static void executeTargetSum() {
+        assertEquals(3, TargetSum.topDown(new int[]{1, 1, 2, 3}, 1));
+        assertEquals(0, TargetSum.topDown(new int[]{1}, 2));
+        //assertEquals(256, TargetSum.topDown(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 1}, 1));
+    }
+
+    private static void executeCountOfSubsetSumWithDifference() {
+        assertEquals(3, CountOfSubsetSumWithDifference.topDown(new int[]{1, 1, 2, 3}, 1));
+        assertEquals(0, CountOfSubsetSumWithDifference.topDown(new int[]{1}, 2));
+
+        //assertEquals(3, CountOfSubsetSumWithDifference.recursion(new int[]{1, 1, 2, 3}, 1));
+        assertEquals(0, CountOfSubsetSumWithDifference.recursion(new int[]{1}, 2));
+    }
+
+    private static void executeMinimumSubsetSumSequence() {
+        assertEquals(4, MinimumSubsetSumSequence.topDown(new int[]{1, 2, 7}));
+        assertEquals(0, MinimumSubsetSumSequence.topDown(new int[]{1, 4, 5}));
+        assertEquals(1, MinimumSubsetSumSequence.topDown(new int[]{12, 1, 10}));
+    }
+
+    private static void executeCountOfSubsetSum() {
+        int[] inputs = new int[]{2, 3, 5, 6, 8, 10};
+        assertEquals(3, CountOfSubsetSum.topDown(inputs, 10));
+        //assertEquals(3, CountOfSubsetSum.recursion(inputs, 10, inputs.length - 1));
     }
 
     private static void executeSubsetSum() {
         int[] inputs = new int[]{2, 3, 7, 8, 11};
-        Arrays.stream(inputs).forEach(element -> assertTrue(SubsetSum.topDown(inputs, element)));
-        assertTrue(subsetSumTopdown(inputs, 11));
-        assertTrue(subsetSumTopdown(inputs, 2));
-        assertTrue(subsetSumTopdown(inputs, 0));
 
-        assertFalse(subsetSumTopdown(inputs, 100));
-        assertFalse(subsetSumTopdown(inputs, 1));
-        assertFalse(subsetSumTopdown(inputs, 32));
+        int[] trueResults = {2, 0, 3, 7, 8, 11};
+        Arrays.stream(trueResults).forEach(expectedResult -> assertTrue(executeSubsetSumTopdown(inputs, expectedResult)));
+        //Arrays.stream(trueResults).forEach(expectedResult -> assertTrue(executeSubsetSumRecursive(inputs, expectedResult)));
+
+        int[] falseResults = {100, 1, 32};
+        Arrays.stream(falseResults).forEach(expectedResult -> assertFalse(executeSubsetSumTopdown(inputs, expectedResult)));
+        Arrays.stream(falseResults).forEach(expectedResult -> assertFalse(executeSubsetSumRecursive(inputs, expectedResult)));
     }
 
     private static void executeEqualSumPartition() {
-        assertTrue(equalSumPartitionRecursive(new int[]{1, 5, 11, 5}));
-        assertTrue(equalSumPartitionRecursive(new int[]{0, 5, 10, 5}));
-        assertTrue(equalSumPartitionRecursive(new int[]{3, 5, 13, 5}));
-        assertTrue(equalSumPartitionTopdown(new int[]{1, 5, 11, 5}));
-        assertTrue(equalSumPartitionTopdown(new int[]{0, 5, 10, 5}));
-        assertTrue(equalSumPartitionTopdown(new int[]{3, 5, 13, 5}));
+        int[][] trueInputs = {
+                {1, 5, 11, 5},
+                {0, 5, 10, 5},
+                {3, 5, 13, 5}
+        };
+        Arrays.stream(trueInputs).forEach(input -> assertTrue(equalSumPartitionRecursive(input)));
+        Arrays.stream(trueInputs).forEach(input -> assertTrue(equalSumPartitionTopdown(input)));
 
-        assertFalse(equalSumPartitionRecursive(new int[]{2, 5, 11, 5}));
-        assertFalse(equalSumPartitionRecursive(new int[]{1, 5, 10, 5}));
-        assertFalse(equalSumPartitionRecursive(new int[]{3, 5, 1, 5}));
-        assertFalse(equalSumPartitionTopdown(new int[]{2, 5, 11, 5}));
-        assertFalse(equalSumPartitionTopdown(new int[]{1, 5, 10, 5}));
-        assertFalse(equalSumPartitionTopdown(new int[]{3, 5, 1, 5}));
+
+        int[][] falseInputs = {
+                {2, 5, 11, 5},
+                {1, 5, 10, 5},
+                {3, 5, 1, 5}
+        };
+        Arrays.stream(falseInputs).forEach(input -> assertFalse(equalSumPartitionRecursive(input)));
+        Arrays.stream(falseInputs).forEach(input -> assertFalse(equalSumPartitionTopdown(input)));
     }
 
     private static void executeSolve01Knapsack() {
-        List<Item> items = List.of(new Item(1, 1), new Item(2, 6), new Item(3, 10), new Item(5, 16));
+        List<Item> items = List.of(
+                new Item(1, 1)
+                , new Item(2, 6)
+                , new Item(3, 10)
+                , new Item(5, 16));
+
         assertTrue(solve01KnapsackRecursive(items, 7) == 22);
         assertTrue(solve01KnapsackMemoized(items, 7) == 22);
         assertTrue(solve01KnapsackTopDown(items, 7) == 22);
