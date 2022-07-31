7cde72e87529be4c21c5f167237440a2e10e1bb0
tapankumar23 <sabat.tapan@gmail.com>
2022-07-31 19:39:06 +0800

    updated 3

--- src/main/java/com/practice/problems/amex/Problem3.java
+++ src/main/java/com/practice/problems/amex/Problem3.java
@@ -1,12 +1,71 @@
 package com.practice.problems.amex;
 
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Problem3 {
 
-    public static void main(String[] args) {
+    static List<String> months = List.of("2020-01-01", "2020-02-01", "2020-03-01", "2020-04-01", "2020-05-01", "2020-06-01",
+            "2020-07-01", "2020-08-01", "2020-09-01", "2020-10-01", "2020-11-01", "2020-12-01");
 
+    public static void main(String[] args) {
+        int[] A;
+        String[] D;
+
+        A = new int[]{100, 100, 100, -10};
+        D = new String[]{"2020-12-31", "2020-12-22", "2020-12-03", "2020-12-29"};
+        solve(A, D);
     }
 
-    private static void solve() {
+    private static int solve(final int[] A, final String[] D) {
+        if (A == null || D == null || A.length != D.length) throw new IllegalArgumentException("Incorrect input");
 
+        Map<LocalDate, List<Transaction>> byMonth = IntStream.range(0, A.length)
+                .mapToObj(i -> new Transaction(A[i], LocalDate.parse(D[i])))
+                .collect(Collectors.groupingBy(tnx -> tnx.date()
+                        .with(TemporalAdjusters.firstDayOfMonth())));
+
+
+        Map<String, Integer> results = new HashMap<>();
+        for (Map.Entry<LocalDate, List<Transaction>> entry : byMonth.entrySet()) {
+            AtomicInteger total = new AtomicInteger(0);
+            AtomicInteger totalCardAmount = new AtomicInteger(0);
+            AtomicInteger cardCount = new AtomicInteger(0);
+            System.out.println("\n" + entry.getKey() + ">>>>>>>>>>>");
+            entry.getValue().forEach(tnx -> {
+                if (tnx.amount() < 0) {
+                    totalCardAmount.addAndGet(tnx.amount());
+                    cardCount.incrementAndGet();
+                }
+                total.addAndGet(tnx.amount());
+                System.out.println(tnx);
+            });
+
+            if (Math.abs(totalCardAmount.get()) < 100 && cardCount.get() < 3) {
+                total.addAndGet(-5);
+            }
+            results.put(entry.getKey().toString(), total.get());
+        }
+
+
+        AtomicInteger totalResult = new AtomicInteger(0);
+        for (String month : months) {
+            if (results.containsKey(month)) {
+                totalResult.addAndGet(results.get(month));
+            } else {
+                totalResult.addAndGet(-5);
+            }
+        }
+
+        return totalResult.get();
+    }
+
+    record Transaction(int amount, LocalDate date) {
     }
 }
